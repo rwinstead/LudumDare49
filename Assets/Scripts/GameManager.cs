d727a499f94c29f32ad6f41f6ee85278165fe3fc
firequake <firequake@users.noreply.github.com>
2021-10-03 00:41:50 -0500

    Integrated scenes & wired room cameras

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -22,14 +22,25 @@ public class GameManager : MonoBehaviour
 
     public bool shieldsActive = false;
 
+    public GameObject NavTablet;
+    public Camera mainCamera;
 
-    public string playerCurrentRoom = "Control Room";
+    public enum Room {ControlRoom, ReactorRoom, CoolantSystem, WasteDisposal, UraniumStorage, Mainframe}
+    
+    public Room playerCurrentRoom = Room.ControlRoom;
 
     public bool Reactor_alarming = false;
     public bool Coolant_alarming = false;
     public bool Waste_alarming = false;
     public bool Storage_alarming = false;
     public bool Mainframe_alarming = false;
+
+    public GameObject cameraAnchor_ControlRoom;
+    public GameObject cameraAnchor_ReactorRoom;
+    public GameObject cameraAnchor_CoolantSystem;
+    public GameObject cameraAnchor_WasteDisposal;
+    public GameObject cameraAnchor_UraniumStorage;
+    public GameObject cameraAnchor_Mainframe;
 
 
     private void Awake()
@@ -60,6 +71,46 @@ public class GameManager : MonoBehaviour
     public void ActivateShields()
     {
         shieldsActive = true;
+    }
+
+    public void OpenNavTablet()
+    {
+        Debug.Log("Open Nav Tablet!");
+        NavTablet.SetActive(true);
+        NavTablet.GetComponentInChildren<NavTabletHandler>().OnOpenTablet();
+        
+    }
+
+    public void SetRoom(Room newRoom)
+    {
+        
+        Debug.Log("GM Setting Up For " + newRoom);
+
+        if (newRoom == Room.ControlRoom)
+        {
+            mainCamera.transform.position = cameraAnchor_ControlRoom.transform.position;
+        }
+        if (newRoom == Room.ReactorRoom)
+        {
+            mainCamera.transform.position = cameraAnchor_ReactorRoom.transform.position;
+        }
+        if (newRoom == Room.CoolantSystem)
+        {
+            mainCamera.transform.position = cameraAnchor_CoolantSystem.transform.position;
+        }
+        if (newRoom == Room.WasteDisposal)
+        {
+            mainCamera.transform.position = cameraAnchor_WasteDisposal.transform.position;
+        }
+        if (newRoom == Room.UraniumStorage)
+        {
+            mainCamera.transform.position = cameraAnchor_UraniumStorage.transform.position;
+        }
+        if (newRoom == Room.Mainframe)
+        {
+            mainCamera.transform.position = cameraAnchor_Mainframe.transform.position;
+        }
+
     }
 
     private void Update()
